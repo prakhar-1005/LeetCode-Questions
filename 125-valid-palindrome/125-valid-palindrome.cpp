080cc439372f4f275df6faa873d3fda52b4ed80f
Prakhar Pandey <94559623+prakhar-1005@users.noreply.github.com>
2022-05-21 00:22:54 +0530

    Time: 4 ms (84.35%), Space: 7.7 MB (21.94%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -0,0 +1,45 @@
+class Solution {
+    private:
+    char low(char c){
+        char temp;
+        if((c>='a' && c<='z') || (c>='0' && c<='9') )
+            return c;
+        else 
+            temp=c-'A' +'a';
+        return temp;
+    }
+    
+     bool valid(char c){
+        if((c>='a'&&c<='z')||(c>='A'&&c<='Z')||(c>='0'&&c<='9'))
+            return 1;
+        else 
+            return 0;
+    }
+    
+public:
+    bool isPalindrome(string s) {
+     string t="";
+        for(int j=0;j<s.length();j++){
+        if(valid(s[j]))
+            t.push_back(s[j]);
+        }
+        
+        
+        for(int i=0;i<t.length();i++)
+             t[i]=low(t[i]);
+        
+        
+    int b=0,e=t.length()-1;
+        while(b<=e){
+            if(t[b]!=t[e])
+                return 0; 
+            else {
+            b++;
+            e--;
+            }
+        }
+        
+        return 1;
+        
+    }
+};
