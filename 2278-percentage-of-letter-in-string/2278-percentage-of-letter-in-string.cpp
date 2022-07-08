b04d6955a1daa4445e2218020ef285c03f3ddf6e
Prakhar Pandey <94559623+prakhar-1005@users.noreply.github.com>
2022-07-09 00:46:10 +0530

    Time: 0 ms (100.00%), Space: 6 MB (83.18%) - LeetHub

--- 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.cpp
+++ 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.cpp
@@ -6,7 +6,10 @@ public:
             if(ch==letter)
                 count++;
         }
-         return (count*100)/s.size();
+/*writing the output like this i.e. multipying by 100 and then dividing by size is important as the return type
+is integer and the datatype of count and s.size() is also integer so dividing them should also return an integer if we write (count/s.size())*100 then count/s.size() will give a fraction which will be considered as 0 as it is an integer.*/        
+        
+        return (count*100)/s.size(); 
         
     }
 };
