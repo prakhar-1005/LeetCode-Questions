07cbc8dccd53c8844c6220e5ab57c8f61b39e98f
Prakhar Pandey <94559623+prakhar-1005@users.noreply.github.com>
2022-07-10 05:09:24 +0530

    Time: 18 ms (74.10%), Space: 11.1 MB (99.81%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -1,13 +1,16 @@
-class Solution {
+class Solution 
+{
 public:
-    int diagonalSum(vector<vector<int>>& mat) {
-        int sum=0;
-        for(int i=0;i<mat.size();i++){
-            for(int j=0;j<mat.size();j++){
-                if(i==j ||i+j==mat.size()-1 )
-                    sum=sum+mat[i][j];
-            }
+    int diagonalSum(vector<vector<int>>& mat)
+    {
+        int x = 0, y = 0, n = mat.size();
+        for(int i = 0 ; i < n ; ++i )
+        {
+            x += mat[i][i];//Primary
+            y += mat[i][n - i - 1];//Secondary
         }
-        return sum;
+        if(n%2==0)
+            return x+y;
+        return x+y-mat[n/2][n/2];//Removes the central element as we have added it twice
     }
 };
