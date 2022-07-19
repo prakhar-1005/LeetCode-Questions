6556f4d889676e03e0ff78a629b5f583ec595321
Prakhar Pandey <94559623+prakhar-1005@users.noreply.github.com>
2022-07-19 16:22:11 +0530

    Time: 281 ms (7.85%), Space: 93.3 MB (89.43%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    
+       int maxProfit(vector<int>& arr) {
+
+        int maxPro=0,mn=INT_MAX;
+        
+        for(int i=0;i<arr.size();i++)
+        { 
+            mn=min(mn,arr[i]);
+            maxPro=max(maxPro,arr[i]-mn);
+         
+        }
+        return maxPro;
+    } 
+    
+};
