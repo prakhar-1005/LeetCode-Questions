3ea112c331a60cb773f3077da4ec0bf8a1a4bc77
Prakhar Pandey <94559623+prakhar-1005@users.noreply.github.com>
2022-08-02 20:45:34 +0530

    Time: 109 ms (25.34%), Space: 26.2 MB (17.48%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
@@ -1,25 +1,15 @@
 class Solution {
-    
-    bool solve(string s){
-        int b=0;
-        int e=s.size()-1;
-        while(b<e){
-            if(s[b++]!=s[e--])
-                return false;
-        }
-        return true;
-    }
-    
 public:
-    
     string firstPalindrome(vector<string>& words) {
-        string ans="";
-        for(int i=0;i<words.size();i++){
-            if(solve(words[i])==true){
-                ans=words[i];
-                break;
-            }
+        for(int i = 0;i<words.size();i++)
+        {
+            string res = words[i];
+            string temp = res;
+            
+            reverse(temp.begin(),temp.end());
+            if(res == temp)
+                return res;
         }
-        return ans;
+        return "";
     }
 };
