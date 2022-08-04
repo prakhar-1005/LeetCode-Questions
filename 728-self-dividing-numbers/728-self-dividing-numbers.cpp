875a459e907c299bf8f34f51c49ac914a7d4b60d
Prakhar Pandey <94559623+prakhar-1005@users.noreply.github.com>
2022-08-05 00:46:47 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (69.54%) - LeetHub

---
+++ 728-self-dividing-numbers/728-self-dividing-numbers.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> ans;
+        for(int i=left;i<=right;i++){
+            int temp=i;
+            int flag=1;
+            while(temp>0){
+                int digit=temp%10;
+                
+                if(digit==0){
+                    flag=0;
+                    break;
+                }
+                
+                if(i%digit!=0){
+                    flag=0;
+                    break;
+                }
+                
+                temp=temp/10;
+            }
+            if(flag)
+            ans.push_back(i);
+        }
+        return ans;
+    }
+};
