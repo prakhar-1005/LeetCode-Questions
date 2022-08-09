b49cc91643111e243c2ccc5a359574f5aa3c0558
Prakhar Pandey <94559623+prakhar-1005@users.noreply.github.com>
2022-08-09 17:57:49 +0530

    Time: 49 ms (28.15%), Space: 31.2 MB (53.59%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sum=0;
+        int lsum=0;
+        
+        for(auto x:nums)
+            sum+=x;
+        
+        for(int i=0;i<nums.size();i++){
+            
+            if(lsum==sum-lsum-nums[i])
+                return i;
+            lsum+=nums[i];     
+        }
+        return -1;
+    }
+};
